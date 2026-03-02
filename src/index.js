56393cfe2522e3b892d394f0fac6af4e64136ccb
MCurb <marcoscurbeco03@gmail.com>
2026-03-02 12:07:33 -0600

    feat: add animation to header and main content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles.css';
+
+const items = document.querySelectorAll('.project-card');
+
+export const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+items.forEach((item) => observer.observe(item));
