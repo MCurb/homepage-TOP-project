75fb14a8acb52862a021e98de911e6bf3af0c775
MCurb <marcoscurbeco03@gmail.com>
2026-03-02 15:23:10 -0600

    feat: add animation to footer contact section

--- src/index.js
+++ src/index.js
@@ -16,3 +16,34 @@ export const observer = new IntersectionObserver(
 );
 
 items.forEach((item) => observer.observe(item));
+
+const contactObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setDelay();
+      }
+    });
+  },
+  {
+    threshold: 0.15,
+  },
+);
+
+const contact = document.querySelector('.contact');
+
+contactObserver.observe(contact);
+
+function setDelay() {
+  const contactElements = document.querySelectorAll('.contact > *');
+
+  let delay = 0.15;
+
+  contactElements.forEach((element) => {
+    delay += 0.1;
+    element.setAttribute(
+      'style',
+      `animation: enter-left 1s ease ${delay}s both`,
+    );
+  });
+}
